31ad2345ad2c722966e803abf46a6072028a6801
Plichta <m.plichta@lancaster.ac.uk>
2021-08-08 19:29:36 +0100

    Entered 32 bit protected mode.
    To show that the code is in 32 bit protected mode, I have added a simple
    hello world message.

--- ExtendedSpace.asm
+++ ExtendedSpace.asm
@@ -1,13 +1,45 @@
 [org 0x7e00]
 
-mov bx, MSG
-call PrintString_16
+jmp EnterProtectedMode
 
-jmp $
-
+%include "gdt.asm"
 %include "Print_16bit.asm"
 
-MSG:
-    db "Extended Space", 0
+EnterProtectedMode:
+    call EnableA20
+    cli
+    lgdt [gdt_descriptor]
 
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    jmp CODE_SEG:StartProtectedMode
+
+EnableA20:
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+    ret
+
+[bits 32]
+
+StartProtectedMode:
+
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov ebx, Message
+    call PrintString
+
+    %include "Print_32bit.asm"
+
+    jmp $
+
+Message:
+    dw "Hello World!!", 0
 times 2048-($-$$) db 0
